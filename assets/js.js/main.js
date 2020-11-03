ffe7354848ab97ee30d1f6231b26e00c24be63e7
Alain LEMARCHAND <alainlemarchand@macbook-pro-de-alain.home>
2020-11-03 15:35:50 +0100

    suite exercice 5 et 6 JS

---
+++ assets/js.js/main.js
@@ -0,0 +1,29 @@
+function divideThem(){
+    var numberOne= document.getElementById('numberOne').value;
+    var numberTwo=document.getElementById('numberTwo').value;
+    alert(numberOne % numberTwo);
+    var result=(numberOne % numberTwo);
+    return (result);
+}
+
+function displayAlert(){
+   var myResult = divideThem();
+   alert(myResult);
+
+}
+
+/* .js de l'exercice 6*/
+
+function multiplyThem(){
+   var shoeSize= document.getElementById('shoeSize').value;
+   var number2= document.getElementById('2').value;
+   alert(shoeSize * number2);
+   var result= (shoeSize * number2);
+   
+   
+
+
+
+
+
+}
